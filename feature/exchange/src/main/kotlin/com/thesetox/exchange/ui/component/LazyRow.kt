dfe568b9ad8b1f062efa015fddb9828d668e7fd2
Stephen Siapno <stephen.siapno7@gmail.com>
2025-06-12 19:04:28 +0800

    Add preview for BalanceLazyRow

--- feature/exchange/src/main/kotlin/com/thesetox/exchange/ui/component/LazyRow.kt
+++ feature/exchange/src/main/kotlin/com/thesetox/exchange/ui/component/LazyRow.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.thesetox.balance.Balance
 import com.thesetox.designsystem.MediumTextStyle
@@ -17,3 +18,13 @@ fun BalanceLazyRow(listOfBalance: List<Balance>) {
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+private fun BalanceLazyRowPreview() =
+    BalanceLazyRow(
+        listOf(
+            Balance(code = "EUR", value = 1000.0),
+            Balance(code = "USD", value = 100.0),
+        ),
+    )
