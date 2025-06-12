b5f67f5c08ef14ff40f79e0de2f59fed3c0698a8
Stephen Siapno <stephen.siapno7@gmail.com>
2025-06-12 18:54:22 +0800

    Add documentation for commission module

--- feature/comission/src/main/java/com/thesetox/comission/CommissionModule.kt
+++ feature/comission/src/main/java/com/thesetox/comission/CommissionModule.kt
@@ -3,6 +3,12 @@ package com.thesetox.comission
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
+/**
+ * Koin module that provides dependencies for the commission feature.
+ *
+ * Currently it exposes a singleton of [GetCommissionUseCase]. Include this
+ * module in your Koin setup to enable commission calculation.
+ */
 val commissionModule =
     module {
         singleOf(::GetCommissionUseCase)
