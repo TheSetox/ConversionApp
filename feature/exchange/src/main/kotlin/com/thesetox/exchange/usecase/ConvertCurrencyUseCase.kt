0215eb708e2516904133581b2eda820688a6ce16
Stephen Siapno <stephen.siapno7@gmail.com>
2025-06-12 19:08:09 +0800

    Add KDoc for ConvertCurrencyUseCase

--- feature/exchange/src/main/kotlin/com/thesetox/exchange/usecase/ConvertCurrencyUseCase.kt
+++ feature/exchange/src/main/kotlin/com/thesetox/exchange/usecase/ConvertCurrencyUseCase.kt
@@ -3,6 +3,13 @@ package com.thesetox.exchange.usecase
 import com.thesetox.exchange.repository.ExchangeRepository
 import java.util.Locale
 
+/**
+ * Converts a given amount from one currency to another.
+ *
+ * The use case obtains the currency rates from [ExchangeRepository] and formats
+ * the result with two decimal digits using the current [Locale]. If the passed
+ * [amount] cannot be parsed, it returns `"0.00"`.
+ */
 class ConvertCurrencyUseCase(private val repository: ExchangeRepository) {
     suspend operator fun invoke(
         amount: String,
