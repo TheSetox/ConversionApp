2f48f9471e6a5db48cb690df8eb64931aa3ce9d8
Stephen Siapno <stephen.siapno7@gmail.com>
2025-06-12 18:47:03 +0800

    Add KDoc for balance module

--- feature/balance/src/main/kotlin/com/thesetox/balance/BalanceModule.kt
+++ feature/balance/src/main/kotlin/com/thesetox/balance/BalanceModule.kt
@@ -3,6 +3,12 @@ package com.thesetox.balance
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
+/**
+ * Koin dependency module exposing balance related use cases.
+ *
+ * Registers [GetDefaultBalanceUseCase] so other components can
+ * easily retrieve the initial wallet contents when the app starts.
+ */
 val balanceModule =
     module {
         singleOf(::GetDefaultBalanceUseCase)
