8e1933ffa951dfa32215f80eb7b6546d9dcf1c82
Stephen Siapno <stephen.siapno7@gmail.com>
2025-06-12 19:12:25 +0800

    Add documentation for GetListOfCurrencyUseCase

--- feature/exchange/src/main/kotlin/com/thesetox/exchange/usecase/GetListOfCurrencyUseCase.kt
+++ feature/exchange/src/main/kotlin/com/thesetox/exchange/usecase/GetListOfCurrencyUseCase.kt
@@ -4,6 +4,13 @@ import com.thesetox.exchange.repository.ExchangeRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
+/**
+ * Use case that emits a list of available currency codes.
+ *
+ * It observes [ExchangeRepository.listOfCurrencyRate] and converts each
+ * [com.thesetox.database.CurrencyRateEntity] to its corresponding currency code.
+ */
+
 class GetListOfCurrencyUseCase(private val repository: ExchangeRepository) {
     operator fun invoke(): Flow<List<String>> {
         return repository.listOfCurrencyRate.map { list -> list.map { it.currencyCode } }
