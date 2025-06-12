50bc4074b875f60aa6148f7abd7a13e53c4638a3
Stephen Siapno <stephen.siapno7@gmail.com>
2025-06-12 18:36:16 +0800

    Add KDoc for database module

--- core/database/src/main/kotlin/com/thesetox/database/DatabaseModule.kt
+++ core/database/src/main/kotlin/com/thesetox/database/DatabaseModule.kt
@@ -1,9 +1,19 @@
+/**
+ * Dependency injection definitions for the database layer of the application.
+ */
 package com.thesetox.database
 
 import androidx.room.Room
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
+/**
+ * Koin module that provides the Room database and its DAO dependencies.
+ *
+ * The module exposes a singleton instance of [ConversionAppDatabase] built with
+ * `Room.databaseBuilder` and a singleton [CurrencyRateDao] retrieved from the
+ * database. These can then be injected throughout the app.
+ */
 val databaseModule =
     module {
         single {
