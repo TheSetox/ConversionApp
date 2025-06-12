7e4eb1651c5fdeba961f4a4d797157bb1adfbc4f
Stephen Siapno <stephen.siapno7@gmail.com>
2025-06-12 19:03:45 +0800

    Add KDoc for ExchangeRepository

--- feature/exchange/src/main/kotlin/com/thesetox/exchange/repository/ExchangeRepository.kt
+++ feature/exchange/src/main/kotlin/com/thesetox/exchange/repository/ExchangeRepository.kt
@@ -3,8 +3,20 @@ package com.thesetox.exchange.repository
 import com.thesetox.database.CurrencyRateEntity
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Repository exposing access to currency exchange rates stored locally.
+ */
 interface ExchangeRepository {
+
+    /**
+     * Continuous stream of all available currency rates.
+     */
     val listOfCurrencyRate: Flow<List<CurrencyRateEntity>>
 
+    /**
+     * Returns the rate information for the given currency code.
+     *
+     * @param code ISO currency code (e.g. "EUR").
+     */
     suspend fun getCurrencyRate(code: String): CurrencyRateEntity
 }
