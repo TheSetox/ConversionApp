cb9dc4b906490e106641aab87cf91f283b52688c
Stephen Siapno <stephen.siapno7@gmail.com>
2025-06-12 19:16:56 +0800

    Add javadoc for sync module

--- feature/sync/src/main/kotlin/com/thesetox/sync/SyncModule.kt
+++ feature/sync/src/main/kotlin/com/thesetox/sync/SyncModule.kt
@@ -4,6 +4,12 @@ import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
+/**
+ * Koin module that wires up the dependencies used by the sync feature.
+ *
+ * This module provides [SyncDataRepository] as the implementation for [SyncRepository]
+ * and exposes [SyncUseCase] so other modules can trigger synchronization.
+ */
 val syncModule =
     module {
         singleOf(::SyncDataRepository) { bind<SyncRepository>() }
