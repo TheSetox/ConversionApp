56692dc5d25ae4c623cf684c20e4bb26386e17e7
Stephen Siapno <stephen.siapno7@gmail.com>
2025-06-12 18:41:26 +0800

    Add KDoc for CurrencyRateResponse

--- core/network/src/main/kotlin/com/thesetox/network/CurrencyRateResponse.kt
+++ core/network/src/main/kotlin/com/thesetox/network/CurrencyRateResponse.kt
@@ -2,6 +2,14 @@ package com.thesetox.network
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Model of the currency rates received from the remote service.
+ *
+ * @property base the currency used as the reference for all rates
+ * @property date the date when the rates were published
+ * @property rates a map of currency codes to their relative exchange rate
+ */
+
 @Serializable
 data class CurrencyRateResponse(
     val base: String,
