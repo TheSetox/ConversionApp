84849ebf71e862b67034872c42900b338a73f013
Stephen Siapno <stephen.siapno7@gmail.com>
2025-06-12 19:13:43 +0800

    Add KDoc to ExchangeModule

--- feature/exchange/src/main/kotlin/com/thesetox/exchange/ExchangeModule.kt
+++ feature/exchange/src/main/kotlin/com/thesetox/exchange/ExchangeModule.kt
@@ -1,3 +1,10 @@
+/**
+ * Koin module that wires all dependencies required by the exchange feature.
+ *
+ * This module provides the concrete implementation of [ExchangeRepository], the
+ * business use cases for currency conversion and exchange, and finally the
+ * [ExchangeViewModel] used by the UI layer.
+ */
 package com.thesetox.exchange
 
 import com.thesetox.exchange.repository.ExchangeDataRepository
@@ -11,6 +18,7 @@ import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
+/** Koin module that provides repositories, use cases and the [ExchangeViewModel]. */
 val exchangeModule =
     module {
         singleOf(::ExchangeDataRepository) { bind<ExchangeRepository>() }
