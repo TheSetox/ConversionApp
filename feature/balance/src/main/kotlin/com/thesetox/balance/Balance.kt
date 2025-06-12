352e0ceab283ba2be6d88273241f3aed25fcc469
Stephen Siapno <stephen.siapno7@gmail.com>
2025-06-12 18:42:52 +0800

    Add KDoc for Balance data class

--- feature/balance/src/main/kotlin/com/thesetox/balance/Balance.kt
+++ feature/balance/src/main/kotlin/com/thesetox/balance/Balance.kt
@@ -1,5 +1,11 @@
 package com.thesetox.balance
 
+/**
+ * Represents a single entry in the user's wallet.
+ *
+ * @property code ISO currency code such as "EUR" or "USD".
+ * @property value Numeric balance amount for the given currency.
+ */
 data class Balance(
     val code: String,
     val value: Double,
